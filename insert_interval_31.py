2821777d436bd280d29aa151cc065aed606630fa
smaitrey <shreyans.maitrey@gnail.com>
2024-09-03 16:10:48 +0530

    day-31

---
+++ insert_interval_31.py
@@ -0,0 +1,57 @@
+"""
+ Insert Interval
+
+ You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+Note that you don't need to modify intervals in-place. You can make a new array and return it.
+
+
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+
+"""
+
+from typing import List
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n = len(intervals)
+        i = 0
+        res = []
+
+        # Case 1: No overlapping before merging intervals
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+
+        # Case 2: Overlapping and merging intervals
+        while i < n and newInterval[1] >= intervals[i][0]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        res.append(newInterval)
+
+        # Case 3: No overlapping after merging newInterval
+        while i < n:
+            res.append(intervals[i])
+            i += 1
+
+        return res
+
+
+## Test-case
+intervals = [[1,3],[6,9]] 
+newInterval = [2,5]
+expected = [[1,5],[6,9]]
+t1 = Solution()
+result = t1.insert(intervals, newInterval)
+print(f"Test case: {'Passed' if result == expected else 'Failed'} (Expected: {expected}, Got: {result})")
+
+
+
